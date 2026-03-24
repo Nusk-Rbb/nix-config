093cad92ab61170aefac8e3646c4a04e018a65ae
Nusk-Rbb <nuskrbb1741dn@gmail.com>
2026-03-25 03:26:37 +0900

    delete: illogical inputs

--- flake.nix
+++ flake.nix
@@ -9,11 +9,6 @@
         url = "github:nix-community/home-manager/release-25.11";
         inputs.nixpkgs.follows = "nixpkgs";
       };
-
-      illogical-flake = {
-         url = "github:soymou/illogical-flake";
-         inputs.nixpkgs.follows = "nixpkgs";
-       };
     };
 
     outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, illogical-flake, ... }@inputs:
